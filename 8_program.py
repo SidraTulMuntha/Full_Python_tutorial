58e4d77f4796bbc42cacfc7ef3bcbe6682a79031
SidraTulMuntha <85743139+SidraTulMuntha@users.noreply.github.com>
2021-09-02 07:53:57 -0700

    python progarm

---
+++ 8_program.py
@@ -0,0 +1,27 @@
+list =[4,18,7,0,13,20,22,2,43]
+
+#sorting a list 
+list.sort()
+print(list)
+
+#reversing the list 
+list.reverse()
+print(list)
+
+# adding integer to the end of the list
+list.append(1)
+print(list)
+
+# adding an integer to the given position 
+list =[4,18,7,0,13,20,22,2,43]
+list.insert(3,8)
+print(list)
+
+#removing an element from the list
+list.remove(22)
+print(list)
+
+#deleting the elemet at index 3
+list =[4,18,7,0,13,20,22,2,43]
+list.pop(3)
+print(list)
