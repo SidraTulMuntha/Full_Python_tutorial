8de938273da42a60afc4e8a4da6c0ba64f8c1b41
SidraTulMuntha <85743139+SidraTulMuntha@users.noreply.github.com>
2021-09-02 07:52:30 -0700

    python program

---
+++ 7_program.py
@@ -0,0 +1,14 @@
+# capitalizing a word in a string
+p = "hope you all liking this repository and taking advantages of it"
+m = p.capitalize()
+print(m)
+
+# finding a word 
+p = "hope you all liking this repository and taking advantages of it"
+n=p.find("liking")
+print(n)
+
+#replacing  a word with other word
+p = "hope you all liking this repository and taking advantages of it"
+d=p.replace("liking","loving")
+print(d) 
