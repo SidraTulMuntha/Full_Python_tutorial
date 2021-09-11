f1d5b11f7c42dd08797bc9e13beab2c09cb1561e
SidraTulMuntha <85743139+SidraTulMuntha@users.noreply.github.com>
2021-09-11 07:27:39 -0700

    python program
    program to define dictionary .

---
+++ program_18.py
@@ -0,0 +1,31 @@
+'''D1={"set":"unordered and unindexed . no duplicates",
+    "dictionary":"ordered and changeable . no duplicate",
+    "list":"ordered and changeable . allow dupliacte",
+    "tuples":"ordered and unchangeable . allow duplicate"}
+print(D1)
+del D1["set"]
+print(D1)'''
+
+D1={"set":"unordered and unindexed . no duplicates",
+    "dictionary":"ordered and changeable . no duplicate",
+    "list":"ordered and changeable . allow dupliacte",
+    "tuples":"ordered and unchangeable . allow duplicate"}
+
+print("Enter a word") 
+inpdic=input()
+print("the meaning of the word : "+inpdic+" is "+D1[inpdic])   '''D1={"set":"unordered and unindexed . no duplicates",
+    "dictionary":"ordered and changeable . no duplicate",
+    "list":"ordered and changeable . allow dupliacte",
+    "tuples":"ordered and unchangeable . allow duplicate"}
+print(D1)
+del D1["set"]
+print(D1)'''
+
+D1={"set":"unordered and unindexed . no duplicates",
+    "dictionary":"ordered and changeable . no duplicate",
+    "list":"ordered and changeable . allow dupliacte",
+    "tuples":"ordered and unchangeable . allow duplicate"}
+
+print("Enter a word") 
+inpdic=input()
+print("the meaning of the word : "+inpdic+" is "+D1[inpdic]) 
