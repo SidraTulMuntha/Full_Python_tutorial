2e39abd7ac713ab821ebd98ed64b3f85c3670b9b
SidraTulMuntha <85743139+SidraTulMuntha@users.noreply.github.com>
2021-09-11 07:26:05 -0700

    python program
    program to define tuple.

---
+++ program_17.py
@@ -0,0 +1,9 @@
+# A tulple is an immutable data type in python
+a=("apple")
+print(type(a))
+a=("apple",)
+print(type(a))
+# DIFFRENCE BETWEEN TUPLE AND A STRING
+mylist = (1,2,3,3,4)
+mylist.append[3]
+print(mylist)
